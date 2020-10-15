17b86009e842515df01a20236fac8f1160ec43b2
peter <zky3332342053@outlook.com>
2020-10-16 03:03:41 +0800

    添加apidoc自动代码生成web api文档

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Custom apiDoc browser title",
+  "url": "http://192.168.0.109:3000",
+  "name": "demo-1",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-10-15T18:49:27.861Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
